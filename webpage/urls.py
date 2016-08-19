73ac73956bd40e2c0ab08df8b6f3502226ffaff0
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-08-19 10:16:09 +0200

    added new link pattern to prevent possible old links to the application from breaking

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.start_view, name="start"),
+    # following url is needed to keep legacy links to this app alive
+    url(r'^totetiroler/$', views.start_view, name='start'),
     url(r'^imprint/$', views.imprint_view, name='imprint'),
     url(r'^about/$', views.about_view, name='about'),
 ]
